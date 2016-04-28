5712978568fa7bcc067a02974d28d547104bde3d
Mic <mic.cassano@gmail.com>
2016-04-28 13:46:31 +0100

    Move #right logic to Direction class

--- lib/direction.rb
+++ lib/direction.rb
@@ -6,4 +6,22 @@ class Direction
   def initialize(current: :N)
     @current = current
   end
+
+  def right
+    @current = next_direction_right
+  end
+
+  private
+
+  def direction_index
+    DIRECTIONS.index(current)
+  end
+
+  def next_direction_right
+    DIRECTIONS[(direction_index + 1) % DIRECTIONS.length]
+  end
+
+  def next_direction_left
+    DIRECTIONS[(direction_index - 1) % DIRECTIONS.length]
+  end
 end
--- spec/unit/direction_spec.rb
+++ spec/unit/direction_spec.rb
@@ -8,4 +8,26 @@ describe Direction do
       expect(direction.current).to eq(:N)
     end
   end
+
+  describe '#right' do
+    it 'changes the direction from North to East' do
+      direction.right
+      expect(direction.current).to eq(:E)
+    end
+
+    it 'changes the direction from East to South' do
+      2.times { direction.right }
+      expect(direction.current).to eq(:S)
+    end
+
+    it 'changes the direction from South to West' do
+      3.times { direction.right }
+      expect(direction.current).to eq(:W)
+    end
+
+    it 'changes the direction from West to North' do
+      4.times { direction.right }
+      expect(direction.current).to eq(:N)
+    end
+  end
 end
