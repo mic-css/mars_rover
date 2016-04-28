cf90cb9c59cea28833404adcfd4311143ac5e465
Mic <mic.cassano@gmail.com>
2016-04-28 16:57:09 +0100

    Add Point logic for increasing and decreasing a coordinate

--- lib/point.rb
+++ lib/point.rb
@@ -5,4 +5,20 @@ class Point
     @x_coordinate = x_coordinate
     @y_coordinate = y_coordinate
   end
+
+  def increase_x
+    @x_coordinate += 1
+  end
+
+  def decrease_x
+    @x_coordinate -= 1
+  end
+
+  def increase_y
+    @y_coordinate += 1
+  end
+
+  def decrease_y
+    @y_coordinate -= 1
+  end
 end
--- spec/unit/point_spec.rb
+++ spec/unit/point_spec.rb
@@ -12,4 +12,32 @@ describe Point do
       expect(point.y_coordinate).to eq(5)
     end
   end
+
+  describe '#increase_x' do
+    it 'increases the x coordinate by one' do
+      point.increase_x
+      expect(point.x_coordinate).to eq(4)
+    end
+  end
+
+  describe '#decrease_x' do
+    it 'decreases the x coordinate by one' do
+      point.decrease_x
+      expect(point.x_coordinate).to eq(2)
+    end
+  end
+
+  describe '#increase_y' do
+    it 'increases the y coordinate by one' do
+      point.increase_y
+      expect(point.y_coordinate).to eq(6)
+    end
+  end
+
+  describe '#decrease_y' do
+    it 'decreases the y coordinate by one' do
+      point.decrease_y
+      expect(point.y_coordinate).to eq(4)
+    end
+  end
 end
