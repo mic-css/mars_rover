d3617bd7ccd308a3d86d47cd07e86100092d744f
Mic <mic.cassano@gmail.com>
2016-04-29 16:29:39 +0100

    Add script to run sample data

---
+++ bin/sample_data.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+require './lib/command_center'
+
+command_center = CommandCenter.new
+
+puts %q(
+Input:
+5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM)
+
+terrain = command_center.plot_terrain('5 5')
+
+rover_1 = command_center.deploy_rover(terrain, '1 2 N')
+command_center.direct_rover(rover_1, 'LMLMLMLMM')
+
+rover_2 = command_center.deploy_rover(terrain, '3 3 E')
+command_center.direct_rover(rover_2, 'MMRMMRMRRM')
+
+puts %Q(
+Output:
+#{rover_1.position.map(&:to_s).join(' ')}
+#{rover_2.position.map(&:to_s).join(' ')})
