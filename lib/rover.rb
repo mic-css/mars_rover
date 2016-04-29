04f4eaeb293310ac19fcef2fc82719fb08abf0ae
Mic <mic.cassano@gmail.com>
2016-04-29 16:26:58 +0100

    Update Rover #position logic to use Direction symbol

--- lib/rover.rb
+++ lib/rover.rb
@@ -19,8 +19,8 @@ class Rover
     direction.forward(point, terrain.boundary_point)
   end
 
-  def coordinates
-    [x_coordinate, y_coordinate]
+  def position
+    [x_coordinate, y_coordinate, current_direction.to_sym]
   end
 
   private
