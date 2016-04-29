fed059987f647fd0872d3b1ae54f3839dba8b1d8
Mic <mic.cassano@gmail.com>
2016-04-29 15:47:43 +0100

    Add errors to Point class when increasing/decreasing coordinates out of bounds

--- lib/point.rb
+++ lib/point.rb
@@ -7,18 +7,26 @@ class Point
   end
 
   def increase_x(boundary_point)
+    raise 'Out of bounds' if x_coordinate >= boundary_point.x_coordinate
     @x_coordinate += 1
   end
 
   def decrease_x(boundary_point)
+    raise 'Out of bounds' if x_coordinate == 0
     @x_coordinate -= 1
   end
 
   def increase_y(boundary_point)
+    raise 'Out of bounds' if y_coordinate >= boundary_point.y_coordinate
     @y_coordinate += 1
   end
 
   def decrease_y(boundary_point)
+    raise 'Out of bounds' if y_coordinate == 0
     @y_coordinate -= 1
   end
+
+  private
+
+  
 end
--- spec/unit/point_spec.rb
+++ spec/unit/point_spec.rb
@@ -1,45 +1,77 @@
 require 'point'
 
 describe Point do
-  subject(:point) { described_class.new(3, 5) }
+  subject(:point) { described_class.new(1, 1) }
 
-  let(:boundary_point) { described_class.new(5, 5) }
+  let(:boundary_point) { described_class.new(2, 2) }
 
   describe '#initialize' do
     it 'initializes with an x-coordinate' do
-      expect(point.x_coordinate).to eq(3)
+      expect(point.x_coordinate).to eq(1)
     end
 
     it 'initializes with a y-coordinate' do
-      expect(point.y_coordinate).to eq(5)
+      expect(point.y_coordinate).to eq(1)
     end
   end
 
   describe '#increase_x' do
     it 'increases the x-coordinate by one' do
       point.increase_x(boundary_point)
-      expect(point.x_coordinate).to eq(4)
+      expect(point.x_coordinate).to eq(2)
+    end
+
+    context 'when the x-coordinate is at the terrain boundary' do
+      it 'raises an error' do
+        point.increase_x(boundary_point)
+        expect { point.increase_x(boundary_point) }
+          .to raise_error 'Out of bounds'
+      end
     end
   end
 
   describe '#decrease_x' do
     it 'decreases the x-coordinate by one' do
       point.decrease_x(boundary_point)
-      expect(point.x_coordinate).to eq(2)
+      expect(point.x_coordinate).to eq(0)
+    end
+
+    context 'when the x-coordinate is at the terrain boundary' do
+      it 'raises an error' do
+        point.decrease_x(boundary_point)
+        expect { point.decrease_x(boundary_point) }
+          .to raise_error 'Out of bounds'
+      end
     end
   end
 
   describe '#increase_y' do
     it 'increases the y-coordinate by one' do
       point.increase_y(boundary_point)
-      expect(point.y_coordinate).to eq(6)
+      expect(point.y_coordinate).to eq(2)
+    end
+
+    context 'when the y-coordinate is at the terrain boundary' do
+      it 'raises an error' do
+        point.increase_y(boundary_point)
+        expect { point.increase_y(boundary_point) }
+          .to raise_error 'Out of bounds'
+      end
     end
   end
 
   describe '#decrease_y' do
     it 'decreases the y-coordinate by one' do
       point.decrease_y(boundary_point)
-      expect(point.y_coordinate).to eq(4)
+      expect(point.y_coordinate).to eq(0)
+    end
+
+    context 'when the y-coordinate is at the terrain boundary' do
+      it 'raises an error' do
+        point.decrease_y(boundary_point)
+        expect { point.decrease_y(boundary_point) }
+          .to raise_error 'Out of bounds'
+      end
     end
   end
 end
